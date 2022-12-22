04f079f440fece09377bab99bd48036987f287fd
unknown <luthandodidiza197@gmail.com>
2022-12-22 11:53:52 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import { FaSearch } from 'react-icons/fa'
+import Photo from './Photo'
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const mainUrl = `https://api.unsplash.com/photos/`
+const searchUrl = `https://api.unsplash.com/search/photos/`
+
+
+
+
+function App() {
+  const [loading, setLoading] = useState(false)
+  const [photos, setPhotos] = useState([])
+  let url;
+
+  const fetchPhotos = async() => {
+    url = `${mainUrl}${clientID}`
+
+    try{
+      setLoading(true)
+      const res = await fetch(url)
+      const data = await res.json()
+      console.log(data)
+
+    }catch(err){
+      console.log(err)
+      setLoading(false)
+
+    }
+  }
+
+
+  useEffect(() => {
+    fetchPhotos()
+  }, [url])
+  return <h2>stock photos starter</h2>
+}
+
+export default App
